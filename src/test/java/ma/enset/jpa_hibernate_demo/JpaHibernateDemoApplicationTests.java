fcaf3311e6b5826bd09da4fd17a197921ea5fd50
Lenovo_Youssouf <ytangara2003@gmail.com>
2025-04-20 12:24:56 +0100

    Initial commit

---
+++ src/test/java/ma/enset/jpa_hibernate_demo/JpaHibernateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.jpa_hibernate_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
